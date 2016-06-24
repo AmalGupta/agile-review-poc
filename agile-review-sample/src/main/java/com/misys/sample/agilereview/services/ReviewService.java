8c33fbfb0bf626566ad74251bc6c2f7c22e59392
amagupta <amal.gupta@misys.com>
2016-06-24 14:55:28 +0530

    Adding Review Comments

--- agile-review-sample/src/main/java/com/misys/sample/agilereview/services/ReviewService.java
+++ agile-review-sample/src/main/java/com/misys/sample/agilereview/services/ReviewService.java
@@ -7,7 +7,7 @@ package com.misys.sample.agilereview.services;
  * @author amagupta
  *
  */
-public class ReviewService implements Service{/*-?|Sample-Service-review|amagupta|c0|?*/
+public class ReviewService implements Service{
 
 	public void serve() {
 		// TODO Auto-generated method stub
